7d146be6cda6bd6c9d3477602d6bc654739df88b
Kupaki <gercovdaniil21@gmail.com>
2025-11-18 23:23:29 +1000

    MemberDataHardCode

--- mvp_vetka/src/FakeData.tsx
+++ mvp_vetka/src/FakeData.tsx
@@ -1,8 +1,36 @@
-type MemberData = {
+import MuscleITGuy from "@/assets/MuscleITGuy.jpg";
+import RobbyItGuy from "@/assets/RobbyItGuy.png";
+import GordenItSceintist from "@/assets/GordenItSceintist.jpg"
+
+export type MemberData = {
     image:string; //укажем здесь путь изображения
     name:string;
     age:number;
-    company:number;
-    about:number;
+    company:string;
+    about:string;
 }
+
+const membersdata: MemberData[] = [
+{
+    image: MuscleITGuy,
+    name:"Айтишникус",
+    age:45,
+    company:"Лентяево",
+    about:"отобьюсь от любого и защищу любого, за БЕСПАЛТНО!"
+},
+{
+    image: RobbyItGuy,
+    name:"Робби",
+    age:55,
+    company:"Злодяево",
+    about:"За деньги я готов подпортить жизнь людям..."
+},
+{
+    image: GordenItSceintist,
+    name:"Горден Фримен",
+    age:35,
+    company:"Черная меза",
+    about:"Я гениальный ученый, герой сопртовления, \
+     возьми меня на работу пж"
+}]
 
