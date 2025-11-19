8c47cc46befab1babd4d75f59f91fcbd35115d8f
Kupaki <gercovdaniil21@gmail.com>
2025-11-19 23:42:46 +1000

    Merge "frontend"

--- mvp_vetka/src/FakeData.tsx
+++ mvp_vetka/src/FakeData.tsx
@@ -2,12 +2,8 @@ import MuscleITGuy from "@/assets/MuscleITGuy.jpg";
 import RobbyItGuy from "@/assets/RobbyItGuy.png";
 import GordenItSceintist from "@/assets/GordenItSceintist.jpg"
 
-<<<<<<< HEAD
-export type MemberData = {
-=======
 export interface MemberData  {
     id:number
->>>>>>> frontend
     image:string; //укажем здесь путь изображения
     name:string;
     age:number;
@@ -15,26 +11,17 @@ export interface MemberData  {
     about:string;
 }
 
-<<<<<<< HEAD
-const membersdata: MemberData[] = [
-{
-=======
 export const membersdata: MemberData[] = [
 {
     id:1,
->>>>>>> frontend
     image: MuscleITGuy,
     name:"Айтишникус",
     age:45,
     company:"Лентяево",
     about:"отобьюсь от любого и защищу любого, за БЕСПАЛТНО!"
 },
-<<<<<<< HEAD
-{
-=======
 {   
     id:2,
->>>>>>> frontend
     image: RobbyItGuy,
     name:"Робби",
     age:55,
@@ -42,10 +29,7 @@ export const membersdata: MemberData[] = [
     about:"За деньги я готов подпортить жизнь людям..."
 },
 {
-<<<<<<< HEAD
-=======
     id:3,
->>>>>>> frontend
     image: GordenItSceintist,
     name:"Горден Фримен",
     age:35,
