0d8c13388b535b37a8a51313af91b679c256c6b7
Kupaki <gercovdaniil21@gmail.com>
2025-12-04 01:10:50 +1000

    Loading screen style changes

--- mvp_vetka/src/LoadingScreen.tsx
+++ mvp_vetka/src/LoadingScreen.tsx
@@ -3,10 +3,32 @@ import './LoadingScreen.css'
 
 const LoadingScreen: React.FC = () => {
     return (
-        <div className="loading-screen">
-            <div className='loading-spinner'>
+        <body>
+            <div className='loading-screen'>
+                <div className = 'loading-logo'>
+                    <div className='inner-square'> 
+                        <svg className="trapezoid-shape"width="40%" height="40%" viewBox="0 0 400 200">
+
+                        <path
+                            d="M 20 20 Q 20 50, 400 0 L 300 200 L 100 200 Z"
+                            fill="#ffa600"
+                            
+                        />
+
+                        <path
+                        d="M 90 200 Q 200 230, 320 200" 
+                        fill="none" 
+                        stroke="#000000" 
+                        stroke-width="30" 
+                        stroke-linecap="round" 
+                        />
+                    </svg>
+                        <div className='dollar-sign'><strong>$</strong></div>
+                    </div>
+                </div>
+                <div className = 'loading-text'> <strong>VETKA</strong> </div>
             </div>
-        </div>
+        </body>
     );
 };
 
